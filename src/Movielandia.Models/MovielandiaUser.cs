e29bda5e5ff006d5b4109b6ba80ae66106fb0876
Valentinles <valentinvn@abv.bg>
2019-04-30 13:12:39 +0300

    project structure added

---
+++ src/Movielandia.Models/MovielandiaUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Movielandia.Models
+{
+    // Add profile data for application users by adding properties to the MovielandiaUser class
+    public class MovielandiaUser : IdentityUser
+    {
+        public string Address { get; set; }
+    }
+}
